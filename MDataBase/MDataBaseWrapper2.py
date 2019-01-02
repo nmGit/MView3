6a1a6f06c73c46ae0110f65d1d6cf1392609a585
Noah Meltzer <nmeltzer@wisc.edu>
2019-01-02 13:53:32 -0600

    Depricated

--- MDataBase/MDataBaseWrapper2.py
+++ MDataBase/MDataBaseWrapper2.py
@@ -11,25 +11,18 @@ __status__ = "Beta"
 from MDataBase import MDataBase
 
 from PyQt4.QtCore import QObject, pyqtSignal, pyqtSlot, QThread
-class MDataBaseWrapper2(QThread):
+class MSQLiteDataBaseWrapper(QThread):
     db_params_updated_signal =pyqtSignal(str, name="db_params_updated_signal")
     db_close_signal = pyqtSignal(name = "db_close_signal")
-    db_save_signal = pyqtSignal(name = "db_save_signal")
+    db_insert_signal = pyqtSignal(str, list, list, name = "db_insert_signal")
     def __init__(self, log_path):
         super(MDataBaseWrapper2, self).__init__()
-
-        #print "initializing MDataBase Wrapper",int(QThread.currentThreadId())
-        #traceback.print_stack()
-
 
         self.db = None
 
         self.db_params_updated_signal.connect(self.openDb)
         self.db_close_signal.connect(self.__close)
-
-
-        # Make sure logging is enabled
-
+        self.db_insert_signal.connect(self.__insert)
         self.openDb(log_path)
 
     def getVariables(self, table_name):
@@ -41,18 +34,52 @@ class MDataBaseWrapper2(QThread):
     def openDb(self, log_path):
         self.db = MDataBase(log_path)
 
+    def insert(self, table, columns, rows):
+        self.db_insert_signal.emit(table, columns, rows)
+
+    def __insert(self, table, columns, rows):
+        self.sql_insert(table, columns, rows)
+
+    def __sql_insert(self, table, columns, rows):
+        if not self.db.dave(table, columns, rows):
+            if not self.db.does_table_exist(table):
+
+
     def query(self, table, fields, *args):
+        '''
+        Returns the result of a query
+        :param table: The name of the table to query
+        :param fields: The fields to query
+        :param args:
+        +------------+------------+-----------+-------------+-----------------------------------------------+
+        | arg[0]     | arg[1]                 |arg[2]|arg[3]| Description                                   |
+        +============+========================+=====+=======+===============================================+
+        | "all"      |            -                         | Grab all data in specified columns            |
+        +------------+------------------------+-----+-------+-----------------------------------------------+
+        | "last"     |            -                         | Grab last committed data in specified columns |
+        +------------+------------------------+-----+-------+-----------------------------------------------+
+        | "range"    | Field over which       |     |       |                                               |
+        |            | the range is specified | Min | Max   | Return everything within the specified range  |
+        +------------+------------+-----------+-------------+-----------------------------------------------+
+        :return: The query response
+        '''
         return self.db.query(table, fields, *args)
+
     def getTables(self):
         return self.db.getTables()
+
+    def isOpen(self):
+        return self.db.isOpen()
+
     def configure_data_sets(self):
         pass
 
     def close(self):
         print "Closing database..."
         self.db_close_signal.emit()
+
     def __close(self):
-        if self.db:
+        if self.db and self.db.isOpen():
             self.db.closeDataSet()
    # def __threadSafeClose(self):
 
