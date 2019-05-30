98de710d0fe884bee1738ddfc21a8404dbf6f1f0
ADR3\lab_user <labrad@physics.wisc.edu>
2019-05-29 19:32:03 -0500

    Improve the sizing of the data viewer

--- MPlugins/MDataViewer.py
+++ MPlugins/MDataViewer.py
@@ -7,12 +7,12 @@ from pyqtgraph.dockarea import *
 import os
 
 class MDataViewer(QtGui.QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, **kwargs):
         super(MDataViewer, self).__init__(parent)
         #root = "C:\Users\Noah\Documents\scripts"
         root = "C:\\Users\\Noah\\Documents\\scripts"
         self.setStyle(QtGui.QStyleFactory.create("plastique"))
-
+        self.setSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
         #self.splitter = QtGui.QSplitter(QtCore.Qt.Vertical)
         self.splitter = QtGui.QSplitter()
         self.splitter.setOrientation(QtCore.Qt.Vertical)
@@ -37,15 +37,16 @@ class MDataViewer(QtGui.QWidget):
         self.colView.clicked.connect(self.item_selected)
         #self.colView.setSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Maximum)
         self.splitter.addWidget(self.colView)
-        self.splitter.setStretchFactor(0,1)
+        #self.splitter.setStretchFactor(0,1)
         self.setWindowTitle("Data Viewer")
-        self.resize(800, 400)
+        
 
         self.db = None
 
         self.area = DockArea()
+        self.area.setSizePolicy(QtGui.QSizePolicy.Ignored, QtGui.QSizePolicy.Ignored)
         self.splitter.addWidget(self.area)
-
+        self.splitter.setSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
         self.main_layout = QtGui.QVBoxLayout()
 
         #self.main_frame = QtGui.QFrame(self)
@@ -57,27 +58,31 @@ class MDataViewer(QtGui.QWidget):
                            "background-color:rgb(%d,%d,%d)}" % (bckgrnd[0], bckgrnd[1], bckgrnd[2]))
         self.main_layout.addWidget(self.splitter)
         self.setLayout(self.main_layout)
-        #self.main_frame.setSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Maximum)
+        #self.setSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Preferred)
 
 #        self.setCentralWidget(self.splitter)
 
-#        self.area.setSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.MinimumExpanding)
+     
         self.colView.setMinimumHeight(0.1*self.size().height())
-        self.colView.setSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Minimum)
+        self.colView.setSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Maximum)
         bckgrnd = web.color_scheme["dark"]["2nd background"]
         self.colView.setStyleSheet("QFrame{"
                            "margin:0.4em; "
                            "border:0.1em solid rgb(0, 0, 0); "
                            "background-color:rgb(%d,%d,%d)}" % (bckgrnd[0], bckgrnd[1], bckgrnd[2]))
-        self.splitter.setStretchFactor(1, 1000)
-        self.splitter.setStretchFactor(2, 1)
+        #self.splitter.setStretchFactor(1, 2)
+        #self.splitter.setStretchFactor(2, 1)
         self.splitter.setStyleSheet("QSplitter::handle\
         {\
             border: 0.2em dotted rgb(150, 150, 180);\
             margin: 0.5em\
         };")
-
-
+      #  self.resize(500, 500)
+       # self.area.resize(500,500)
+       # self.splitter.resize(500,500)
+        max_height = kwargs.get("max_height",None)
+        if max_height != None:
+            self.setMaximumHeight(max_height)
 
     def item_selected(self, index):
 
@@ -94,19 +99,25 @@ class MDataViewer(QtGui.QWidget):
         else:
             return
 
-        dock = Dock(file_path, size=(500, 300), closable=True)
-        self.area.addDock(dock, 'bottom')
+        dock = Dock(file_path, closable=True)
+        
 
         self.graph = MGrapher(self, live_mode=False)
         self.graph.show_plot()
-        self.graph.setSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
-        self.graph.resize(self.size().width(), 0.9*self.size().height())
+        
+        
+        #self.area.resize(0.5*self.size().width(), 0.5*self.size().height())
+       
         self.graph.setStyle(QtGui.QStyleFactory.create("plastique"))
-
-
+      
+        
         dock.addWidget(self.graph)
-
-
+        #self.graph.resi
+        #self.graph.resize(self.graph.width(), self.graph.width())
+        #dock.setSizePolicy( QtGui.QSizePolicy.Maximum,  QtGui.QSizePolicy.Maximum)
+        #dock.setStretch(self.graph.width(), self.graph.width())
+        #dock.resize(500,300)
+        self.area.addDock(dock, 'bottom')
         tables = self.db.getTables()
         print "Tables are:", tables
         data = []
@@ -120,7 +131,8 @@ class MDataViewer(QtGui.QWidget):
                 curve = self.graph.add_curve(column)
                 curve.set_data(times[-1], data[-1])
                 #print "Adding", times[-1], data[-1]
-
+        #self.setSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
+        #self.setFixedHeight(50)
 if __name__ == '__main__':
 
     import sys
